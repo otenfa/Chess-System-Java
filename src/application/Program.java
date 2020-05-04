8387181d04c1384a551099371ed117bbcd8eab2d
otenfa <afneto1960@gmail.com>
2020-05-04 15:32:30 -0300

    First class: Positon

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import Bordergame.Positon;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello word!!!");
+		Positon pos = new Positon(3, 5);
+		System.out.println(pos);
+		
 		
 	}
 
